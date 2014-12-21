0c9bee37ce25f0399f42d2ce9045783c5918199a
mosutw <luke1974@gmail.com>
2014-12-21 23:18:48 +0800

    ch5-45

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
+
 var app = express();
 
 var handlebars = require('express3-handlebars')
@@ -14,12 +15,29 @@ app.set('view engine', 'handlebars');
 app.set('port',process.env.PORT||3000);
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+  res.locals.showTests = app.get('env') !== 'production' &&
+      req.query.test === '1';
+  next();
+});
+
 app.get('/',function(req,res){
   res.render('home');
 });
 
 app.get('/about',function(req,res){
-  res.render('about',{fortune: fortune.getFortune() });
+  res.render('about',{
+    fortune: fortune.getFortune(),
+    pageTestScript: '/qa/tests-about.js' 
+  });
+});
+
+app.get('/tours/hood-river', function(req, res) {
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res) {
+  res.render('tours/request-group-rate');
 });
 
 app.use(function(req, res, next){
