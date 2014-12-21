552db044501e6a24e4f6e5e24ca16b4d25a56aef
mosutw <luke1974@gmail.com>
2014-12-21 21:30:40 +0800

    initital commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+
+var app = express();
+
+var handlebars = require('express3-handlebars')
+    .create({defaultLayout:'main'});
+
+var fortunes = [
+  "11111",
+  "22222",
+  "33333",
+  "44444",
+  "55555",
+];
+
+app.engine('handlebars',handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+app.set('port',process.env.PORT||3000);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req,res){
+  res.render('home');
+});
+
+app.get('/about',function(req,res){
+  var randomFortune =
+    fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about',{fortune: randomFortune });
+});
+
+app.use(function(req, res, next){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err,req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.');
+});
